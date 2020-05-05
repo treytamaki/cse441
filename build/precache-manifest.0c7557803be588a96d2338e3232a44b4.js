af9bf205130f9c3d14f981057ade987c99a48eb1
Trey Tamaki <tgtamaki@uw.edu>
2020-05-05 14:27:19 -0700

    add react

---
+++ build/precache-manifest.0c7557803be588a96d2338e3232a44b4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b81901ac1b0d5b1a25a425ec8f3fc739",
+    "url": "/cse441/index.html"
+  },
+  {
+    "revision": "72accdf75b9d0341128b",
+    "url": "/cse441/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "17c79084c1e20441ba34",
+    "url": "/cse441/static/js/2.a489c2db.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/cse441/static/js/2.a489c2db.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "72accdf75b9d0341128b",
+    "url": "/cse441/static/js/main.cae8259b.chunk.js"
+  },
+  {
+    "revision": "2cb2239c46e140211ff5",
+    "url": "/cse441/static/js/runtime-main.27214c7f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/cse441/static/media/logo.5d5d9eef.svg"
+  }
+]);
